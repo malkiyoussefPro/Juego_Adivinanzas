3b9f3d5d4fb82efdbfd562a3941c31501fed3e87
Youssef Malki <malki.youssef10@gmail.com>
2023-11-08 21:01:05 +0100

    Correction des erreurs

--- script.js
+++ script.js
@@ -1,97 +1,95 @@
+//creamos array de objetos
 const questions = [
-{
-    question: "que pais tiene mas poblacion?",
-    respuestaCorrecta: "La china",
-    respuestaIncorrecta: "La India",
-},
-{
-    question: "El primer astronauta a attrizar la luna ?",
-    respuestaCorrecta: "neil amstrong",
-    respuestaIncorrecta: "mati amstrong",
-},
-    
-];
-
-let indexQuestionActual  = 0;
+    {
+        questio: "Quin pais té més població?",
+        respostaCorrecta: "La Xina",
+        respostaIncorrecta: "La India",
+    },
+    {
+        questio: "El primer astronàuta a trepitjar la lluna va ser?",
+        respostaCorrecta: "Neil Armstrong",
+        respostaIncorrecta: "Louis Armstrong",
+    },
+    {
+        questio: ""
+    }
+]; // aqui va ; ya que acaba el array
+//variables necesarias
+//variable  para saber en qué parte del array estamos:
+let indexQuestioActual = 0;
+//contadores de respuestas
 let respostesCorrectes = 0;
-let respostesinCorrectes = 0;
+let respostesIncorrectes = 0;
 
-const questioproposada = document.getElementById("questioproposada");
+const questioProposada = document.getElementById("questioProposada");
 const btnEsquerre = document.getElementById("btnEsquerre");
 const btnDret = document.getElementById("btnDret");
 const missatge = document.getElementById("missatge");
-const  btnReiniciar = document.getElementById("btnReiniciar");
+const btnReiniciar = document.getElementById("btnReiniciar");
 
-function mezclaRespuesta(correcta, incorrecta){
-
-    const respuesta = [correcta, incorrecta];
-    respuesta.sort((() => Math.random() - 0.5));
-    return respuesta;
+function barrejaRespostes(correcta, incorrecta) { // para poner las preguntas y respuestas en los lados aleatoriamente
+    const respostes = [correcta, incorrecta];
+    respostes.sort(() => Math.random() - 0.5);
+    return respostes;
 }
 
-function mostrarQuestion(){
+function mostraQuestio() { //comprueba que estamos dentro del array de preguntas
+    if(indexQuestioActual < questions.length) {
+        const questioActual = questions[indexQuestioActual];
+        questioProposada.textContent = questioActual.questio;//coloca la pregunta en el primer div
 
-    if(indexQuestionActual< questions.length){
-        const questionActual = questions[indexQuestionActual];
-        questioproposada.textContent = questionActual.question;
+        const [barrejatCorrecte, barrejatIncorrecte] = barrejaRespostes( // vuelca cada elemento en los botones 
+            questioActual.respostaCorrecta,
+            questioActual.respostaIncorrecta
+        );
 
-      
-        const [mezclaCorrecta, mezclainCorrecta] = mezclaRespuesta(questionActual.respuestaCorrecta, questionActual.respuestaIncorrecta);
+        btnEsquerre.textContent = barrejatCorrecte;
+        btnDret.textContent = barrejatIncorrecte;
+    } else {
+        //el juego ha terminado
+        if(respostesCorrectes === questions.length) {
+            missatge.textContent = "Has guanyat!! Has respost totes les questions correctament!";
+        } else {
+            missatge.textContent = `Joc acabat. Respostes correctes: ${ respostesCorrectes }, Respostes incorrectes: ${ respostesIncorrectes}`;
+        }
 
-        btnEsquerre.textContent = mezclaCorrecta;
-        btnDret.textContent = mezclainCorrecta;
-    }else{
-        // el juego ha acabado
-
-        if(respostesCorrectes === question.length){
-
-            missatge.textContent = "has ganado !! has respondido a las preguntas correctamente";
-        }else{
-            missatge.textContent =`Juego acabado . respuestas correctas : ${respostesCorrectes},
-                                    Respuestas incorrectas: ${respostesinCorrectes}´;
-        
-      }
-
-        btnEsquerre.style.display = "none";
+        btnEsquerre.style.display = "none"; //ocultamos estos botones y mostramos el de reiniciar
         btnDret.style.display = "none";
-        btnReiniciar.style.display = "none";
-      
+        btnReiniciar.style.display = "block";
     }
-
-    
 }
 
-function comprovaRespuesta(respuestaselecionada){
-    const questionActual = question[indexQuestionActual];
-    if(respuestaselecionada === questionActual.respostesCorrectes){
+//funcion que verifica las respuestas
+function comprovaResposta (respostaSeleccionada) {
+
+    const questioActual = questions[indexQuestioActual];
+
+    if(respostaSeleccionada === questioActual.respostaCorrecta) {
         respostesCorrectes++;
-    }else{
-        respostesinCorrectes++;
+    } else {
+        respostesIncorrectes++;
     }
-    indexQuestionActual++;
 
-    mostrarQuestion();
+    indexQuestioActual++; // pasamos a la pregunta siguiente
 
+    mostraQuestio();
 }
 
-btnEsquerre.addEventListener("click", () => comprovaRespuesta (btnEsquerre.textContent));
-btnDret.addEventListener("click", () => comprovaRespuesta (btnDret.textContent));
-btnReiniciar.addEventListener("click", () => {
-    indexQuestionActual = 0;
+//eventos
+btnEsquerre.addEventListener("click", () => comprovaResposta(btnEsquerre.textContent));
+btnDret.addEventListener("click", () => comprovaResposta(btnDret.textContent));
+
+btnReiniciar.addEventListener("click", () =>{
+    indexQuestioActual = 0;
     respostesCorrectes = 0;
-    respostesinCorrectes = 0;
+    respostesIncorrectes = 0;
     missatge.textContent = "";
     btnEsquerre.style.display = "inline-block";
     btnDret.style.display = "inline-block";
     btnReiniciar.style.display = "none";
-    
-    mostrarQuestion();
 
-
+    mostraQuestio();
 });
 
-//Comenzar el juego 
-
-mostrarQuestion();
-
-
+//Comenzar el juego
+mostraQuestio();
